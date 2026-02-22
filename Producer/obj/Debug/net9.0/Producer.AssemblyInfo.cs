174889e256b4204801baa1a362f1db28063f51bc
josevalencio <valencio.santos@hotmail.com>
2026-02-22 19:42:50 -0300

    bibliotecas

--- Producer/obj/Debug/net9.0/Producer.AssemblyInfo.cs
+++ Producer/obj/Debug/net9.0/Producer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Producer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d535474e638743a458f512fdb901d73254be31b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Producer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Producer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
