5d535474e638743a458f512fdb901d73254be31b
josevalencio <valencio.santos@hotmail.com>
2026-02-22 19:27:28 -0300

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{ HostName = "localhost"};
+
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "letterbox", 
+    durable:false,
+    exclusive:false,
+    autoDelete:false,
+    arguments:null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync +=  async (model, ea) => {
+   var body = ea.Body.ToArray();
+   var message = Encoding.UTF8.GetString(body);
+   Console.WriteLine($"Received message: {message}");
+};
+
+await channel.BasicConsumeAsync(queue: "letterbox", autoAck: true, consumer: consumer);
+
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName = "localhost"};
+
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "letterbox", 
+    durable:false,
+    exclusive:false,
+    autoDelete:false,
+    arguments:null);
+
+var message = "this is my fist Message";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(string.Empty, "letterbox", encodedMessage);
+
+Console.WriteLine($"Published message: {message}");
